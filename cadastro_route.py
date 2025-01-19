f3ef55c19ac46ca946daab4948672bbb41481c97
Laidone <laidonecarvalho@hotmail.com>
2025-01-19 17:06:58 -0300

    Deploy

--- cadastro_route.py
+++
@@ -1,8 +0,0 @@
-from fastapi import FastAPI
-from api.Controller import cadastro
-from api.utils import auth_util
-
-app = FastAPI()
-
-# Rotas de cadastro
-app.include_router(cadastro.router)
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from api.Controller import controller, auth, cadastro
+from api.Controller import controller, auth
 from api.utils import auth_util
 
 app = FastAPI()
@@ -10,6 +10,3 @@ app.include_router(controller.router)
 # Rotas de autenticação
 
 app.include_router(auth.router)
-
-# Rotas de cadastro
-app.include_router(cadastro.router)
